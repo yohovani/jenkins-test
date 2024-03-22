aa9e57de6b7d353c2705e22e7728916dbf35884f
Yohovani Vargas <30984240+yohovani@users.noreply.github.com>
2024-03-22 14:14:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const pokemonData = [
-    { name: 'Pikachu', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png' },
-    { name: 'Bulbasaur', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' },
-    { name: 'Charmander', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png' },
+    { name: 'Pikachu', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png', description: 'Electric mouse Pokémon.' },
+    { name: 'Bulbasaur', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png', description: 'Seed Pokémon.' },
+    { name: 'Charmander', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png', description: 'Lizard Pokémon.' },
     // Añadir más Pokémon aquí
 ];
 
 let currentIndex = 0;
 const pokemonImage = document.getElementById('pokemon-image');
 const pokemonName = document.getElementById('pokemon-name');
+const pokemonDescription = document.getElementById('pokemon-description');
 const prevButton = document.getElementById('prev-btn');
 const nextButton = document.getElementById('next-btn');
 
@@ -16,6 +17,7 @@ function displayPokemon(index) {
     const pokemon = pokemonData[index];
     pokemonImage.src = pokemon.image;
     pokemonName.textContent = pokemon.name;
+    pokemonDescription.textContent = pokemon.description;
 }
 
 // Mostrar el primer Pokémon al cargar la página
